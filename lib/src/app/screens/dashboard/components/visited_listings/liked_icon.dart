4a63062aaa23ac3d147afabb6469aefe3bcfef68
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-16 02:58:30 +0500

    Add liked icon

---
+++ lib/src/app/screens/dashboard/components/visited_listings/liked_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LikedIcon extends StatelessWidget {
+  const LikedIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: -15,
+      left: -35,
+      child: MaterialButton(
+        onPressed: (() {}),
+        color: Colors.green,
+        textColor: Colors.white,
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(5),
+        child: const Icon(Icons.thumb_up, size: 16),
+      ),
+    );
+  }
+}
