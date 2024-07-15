60ae27f18e9113c50299245b3d3125f23c4cace9
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-16 02:59:28 +0500

    add custom icon

---
+++ lib/src/app/screens/dashboard/components/visited_listings/disliked_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DislikedIcon extends StatelessWidget {
+  const DislikedIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: -15,
+      left: -35,
+      child: MaterialButton(
+        onPressed: (() {}),
+        color: Colors.red,
+        textColor: Colors.white,
+        shape: const CircleBorder(),
+        padding: const EdgeInsets.all(5),
+        child: const Icon(Icons.thumb_down, size: 16),
+      ),
+    );
+  }
+}
