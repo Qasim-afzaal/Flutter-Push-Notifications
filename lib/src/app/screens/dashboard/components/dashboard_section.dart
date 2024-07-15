7bdf933a2a11ecef1920196c490c84bdae3942e9
Qasim-afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-16 02:58:47 +0500

    add deshboard section module

---
+++ lib/src/app/screens/dashboard/components/dashboard_section.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class DashboardSection extends StatelessWidget {
+  const DashboardSection({
+    Key? key,
+    required this.child,
+    required this.title,
+    this.backgroundColor = Colors.transparent,
+  }) : super(key: key);
+
+  final Widget child;
+  final String title;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(color: backgroundColor),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: theme.textTheme.titleLarge
+                ?.copyWith(color: theme.colorScheme.onSurface, fontSize: 16),
+          ),
+          const SizedBox(height: 10),
+          child,
+        ],
+      ),
+    );
+  }
+}
