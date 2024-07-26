4568ce54723db106165741cd3b8bc9a6f3e78ac2
Qasim Afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-26 22:45:05 +0500

    Update liked_icon.dart

--- lib/src/app/screens/dashboard/components/visited_listings/liked_icon.dart
+++ lib/src/app/screens/dashboard/components/visited_listings/liked_icon.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LikedIcon extends StatelessWidget {
-  const LikedIcon({Key? key}) : super(key: key);
+  const LikedIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
