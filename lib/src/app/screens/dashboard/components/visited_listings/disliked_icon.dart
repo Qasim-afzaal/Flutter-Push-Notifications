c2f24861fa766b7566c3f5406ba800ab5fa65ec4
Qasim Afzaal <147349016+Qasim-afzaal@users.noreply.github.com>
2024-07-26 22:45:25 +0500

    Update disliked_icon.dart

--- lib/src/app/screens/dashboard/components/visited_listings/disliked_icon.dart
+++ lib/src/app/screens/dashboard/components/visited_listings/disliked_icon.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DislikedIcon extends StatelessWidget {
-  const DislikedIcon({Key? key}) : super(key: key);
+  const DislikedIcon({super.key});
 
   @override
   Widget build(BuildContext context) {
